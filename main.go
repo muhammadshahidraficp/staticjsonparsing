8a7126a8bab332aa79bb3ef22613a128cecd87f5
Shahid <you@example.com>
2019-04-10 10:05:42 +0530

    json parsing data stored in mysql

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func dbConn() (db *sql.DB) {
+
+	dbDriver := "mysql"
+	dbUser := "vinam"
+	dbPass := "vinam"
+	dbName := "goblog"
+
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
 
 var JSON = `{
     "name":"Mark Taylor",
@@ -16,13 +35,22 @@ var JSON = `{
 }`
 
 func main() {
-
 	var info map[string]interface{}
 	json.Unmarshal([]byte(JSON), &info)
-
-	fmt.Println(info["name"])
-	fmt.Println(info["jobtitle"])
-	fmt.Println(info["email"])
-	fmt.Println(info["phone"].(map[string]interface{})["home"])
-	fmt.Println(info["phone"].(map[string]interface{})["office"])
+	name := info["name"]
+	jobtitle := info["jobtitle"]
+	email := info["email"]
+	home := info["phone"].(map[string]interface{})["home"]
+	office := info["phone"].(map[string]interface{})["office"]
+	fmt.Println(name)
+	fmt.Println(jobtitle)
+	fmt.Println(email)
+	fmt.Println(home)
+	fmt.Println(office)
+	db := dbConn()
+	insForm, err := db.Prepare("INSERT INTO employee(name,jobtitle,email,home,office) VALUES(?,?,?,?,?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	insForm.Exec(name, jobtitle, email, home, office)
 }
